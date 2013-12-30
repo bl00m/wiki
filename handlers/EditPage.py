2d5022a8dc52728b92096073b7d1ebcdd5e44100
bl00m <tguchi3@gmail.com>
2013-12-30 12:32:07 -0800

    fixed redirects in edit page

--- handlers/EditPage.py
+++ handlers/EditPage.py
@@ -1,6 +1,7 @@
 from handlers.MainHandler import MainHandler
 from db.post import Post
 import re
+import logging
 
 class EditPage(MainHandler):
     def get(self, page_title):
@@ -9,10 +10,7 @@ class EditPage(MainHandler):
         if self.user and valid_post(title):
             self.redirect('/%s' % title)
 
-        elif not valid_title(page_title):
-            self.redirect('/')
-
-        elif self.user:
+        elif self.user and valid_title(title):
             self.render('editpage.html')
 
         else:
@@ -23,13 +21,8 @@ class EditPage(MainHandler):
         if self.user:
             content = self.request.get('content')
 
-            if not valid_post(title):
-                Post.submit(title, content)
-                self.redirect('/%s' % title)
-
-            else:
-                self.redirect('/%s' % title)
-                
+            Post.submit(title, content)
+            self.redirect('/%s' % title)
         else:
             self.redirect('/')
 
@@ -37,9 +30,9 @@ def page_title_str(page_title):
     return str(page_title.replace('/',''))
 
 def valid_post(page_title):
-    return Post.by_title(page_title) is not None
+    return page_title and Post.by_title(page_title) is not None
 
 def valid_title(title):
-    return TITLE_RE.match(title)
+    return title and TITLE_RE.match(title)
 
 TITLE_RE = re.compile(r'^[a-zA-Z0-9_-]{1,20}$')
